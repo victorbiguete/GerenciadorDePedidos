b3a5a974dabd8e5721b9092d17ca3eb8e14bab8d
victorbiguete <victorbiguete@gmail.com>
2025-11-03 20:43:27 -0300

    feat(domain):adicionado na camada de Domain as Enums

---
+++ src/Backend/MyStore.Domain/Enum/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Domain.Enum
+{
+    public enum OrderStatus
+    {
+        Draft = 0,
+        Created = 1,
+        Confirmed = 2,
+        Shipped = 3,
+        Cancelled = 4
+    }
+}
