ea2ff7823e39a8580a0a7f209a8c799f38567cb4
victorbiguete <victorbiguete@gmail.com>
2025-11-03 20:45:38 -0300

    feat(infrastructure): Criado a propriedade de ConnectionString

---
+++ src/Backend/MyStore.Infrasctructure/Extensions/ConfigurationExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Infrasctructure.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static string ConnectionString(this IConfiguration configuration)
+        {
+            return configuration.GetConnectionString("SqlServerConnection")!;
+        }
+    }
+}
