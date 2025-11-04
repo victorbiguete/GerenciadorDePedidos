68810c3658d1d346d45e3abc9bda814ddc4205d1
victorbiguete <victorbiguete@gmail.com>
2025-11-04 13:55:54 -0300

    feat(infra):Criado as Injeções de Dependencias.

---
+++ src/Backend/MyStore.Infrasctructure/DependencyInjectionExtension.cs
@@ -0,0 +1,71 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyStore.Domain.IRepository.ICustomer;
+using MyStore.Domain.IRepository.Order;
+using MyStore.Domain.IRepository.OrderItens;
+using MyStore.Domain.IRepository.Product;
+using MyStore.Infrasctructure.DataAccess;
+using MyStore.Infrasctructure.DataAccess.MongoDb.Repositories.Customer;
+using MyStore.Infrasctructure.DataAccess.MongoDb.Repositories.Order;
+using MyStore.Infrasctructure.DataAccess.MongoDb.Repositories.OrderItem;
+using MyStore.Infrasctructure.DataAccess.MongoDb.Repositories.Product;
+using MyStore.Infrasctructure.DataAccess.Repositories.Customer;
+using MyStore.Infrasctructure.DataAccess.Repositories.Order;
+using MyStore.Infrasctructure.DataAccess.Repositories.OrderItem;
+using MyStore.Infrasctructure.DataAccess.Repositories.Product;
+using MyStore.Infrasctructure.Extensions;
+using System.Reflection;
+
+namespace MyStore.Infrasctructure
+{
+    public class DependencyInjectionExtension
+    {
+        public static void AddInfrastructure(IServiceCollection services,IConfiguration configuration)
+        {
+            AddRepository(services);
+            AddDbContext_SqlServer(services,configuration);
+            AddFluentMigrator(services, configuration);
+            AddMongoDb(services, configuration);
+        }
+
+        private static void AddDbContext_SqlServer(IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.ConnectionString());
+            });
+        }
+
+        private static void AddRepository(IServiceCollection services)
+        {
+            services.AddScoped<ICustomerReadRepository,CustomerReadRepository>();
+            services.AddScoped<ICustomerUpdateOnlyRepository,CustomerRepository>();
+            services.AddScoped<ICustomerWriteOnlyRepository,CustomerRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductUpdateOnlyRepository,ProductRepository>();
+            services.AddScoped<IProductWriteOnlyRepository,ProductRepository>();
+            services.AddScoped<IOrderReadRepository,OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository,OrderRepository>();
+            services.AddScoped<IOrderUpdateOnlyRepository,OrderRepository>();
+            services.AddScoped<IOrderItemReadRepository,OrderItemReadRepository>();
+            services.AddScoped<IOrderItemUpdateOnlyRepository, OrderItemRepository>();
+            services.AddScoped<IOrderItemWriteOnlyRepository, OrderItemRepository>();
+        }
+
+        private static void AddMongoDb(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<MongoDbContext>();
+        }
+
+        private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddFluentMigratorCore().ConfigureRunner(options =>
+            {
+                options.AddSqlServer().WithGlobalConnectionString(configuration.ConnectionString())
+                .ScanIn(Assembly.Load("MyStore.Infrasctructure")).For.All();
+            });
+        }
+    }
+}
