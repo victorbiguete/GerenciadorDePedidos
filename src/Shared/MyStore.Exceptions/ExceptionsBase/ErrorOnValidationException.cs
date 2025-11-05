a2df52df82b271be53ec02998971b3d8e258c64d
victorbiguete <victorbiguete@gmail.com>
2025-11-05 05:00:32 -0300

    feat(communication): Implementação dos Tipos de Exceptions

---
+++ src/Shared/MyStore.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Exceptions.ExceptionsBase
+{
+    public class ErrorOnValidationException : MyStoreExceptions
+    {
+        public IList<string> ErrorsMessages { get; set; }
+        public ErrorOnValidationException(IList<string> errors) : base(string.Empty)
+        {
+            ErrorsMessages = errors;
+        }
+    }
+}
---
+++ src/Shared/MyStore.Exceptions/ExceptionsBase/MyStoreExceptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Exceptions.ExceptionsBase
+{
+    public class MyStoreExceptions : Exception
+    {
+        public MyStoreExceptions(string? message) : base(message)
+        {
+        }
+    }
+}
