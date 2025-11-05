8f516596929efcef44fd9dcef73fdd947d7a7832
victorbiguete <victorbiguete@gmail.com>
2025-11-05 04:59:17 -0300

    feat(api): implementação dos Filters

---
+++ src/Backend/MyStore.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyStore.Communication.Response;
+using MyStore.Exceptions;
+using MyStore.Exceptions.ExceptionsBase;
+using System.Net;
+
+namespace MyStore.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is MyStoreExceptions)
+                HandleProjectException(context);
+            else
+                ThrowUnknowException(context);
+        }
+
+        private static void HandleProjectException(ExceptionContext context)
+        {
+            if(context.Exception is NotFoundException)
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+                context.Result = new NotFoundObjectResult(new ResponseErrorJson(context.Exception.Message));
+            }
+        }
+
+        private static void ThrowUnknowException(ExceptionContext context)
+        {
+            context.HttpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            context.Result = new ObjectResult(new ResponseErrorJson(ResourceMessagesExceptions.UNKOWN_ERROR));
+        }
+    }
+}
