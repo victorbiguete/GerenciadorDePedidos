0ccc0f15a55210bfdd9b4f36545fa95a80c31210
victorbiguete <victorbiguete@gmail.com>
2025-11-05 04:58:34 -0300

    feat(app): Finalização da Application

---
+++ src/Backend/MyStore.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyStore.Application.Services.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddAutoMapper(services);
+            AddMediatr(services);
+        }
+
+        private static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapping).Assembly);
+        }
+
+        private static void AddMediatr(IServiceCollection services)
+        {
+            services.AddMediatR(options =>
+            {
+                options.RegisterServicesFromAssemblies(typeof(DependencyInjectionExtension).Assembly);
+            });
+        }
+    }
+}
