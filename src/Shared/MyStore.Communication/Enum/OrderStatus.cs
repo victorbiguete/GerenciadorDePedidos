d4790827b968e245392f12ae1c2034d01c5a9b25
victorbiguete <victorbiguete@gmail.com>
2025-11-05 05:02:30 -0300

    feat(communication): Finalização da camada de Communication

---
+++ src/Shared/MyStore.Communication/Enum/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Communication.Enum
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Processing = 1,
+        Completed = 2,
+        Cancelled = 3,
+    }
+}
