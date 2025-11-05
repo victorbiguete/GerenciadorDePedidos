8710d4f2acd185c2d107590ea554b59f8042b2e6
victorbiguete <victorbiguete@gmail.com>
2025-11-05 04:57:58 -0300

    feat(app): Implementação da Commands para OrderItens

---
+++ src/Backend/MyStore.Application/Commands/OrderItens/RegisterOrderItemCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MyStore.Application.Commands.OrderItens
+{
+    public class RegisterOrderItemCommand
+    {
+        [JsonIgnore]
+        public long OrderId { get; set; }
+        public long ProductId { get; set; }
+
+        [JsonIgnore]
+        public string ProductName { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+
+        [JsonIgnore]
+        public decimal UnitPrice { get; set; }
+        [JsonIgnore]
+        public decimal TotalPrice { get; set; }
+    }
+}
